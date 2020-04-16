d3afe486cf63b71add67387c97514c1648559de5
cxnaive <1012033254@qq.com>
2020-04-16 18:08:25 +0800

    reinstall maven

--- src/test/java/cxmc/LuaTest.java
+++ src/test/java/cxmc/LuaTest.java
@@ -11,45 +11,6 @@ import cxmc.h2.*;
  */
 public class LuaTest
 {
-    class TestClass implements PluginDef{
-        private LuaLoader lualoader;
-        private LuaRunner luarunner;
-        private FileLoader fileLoader;
-        private H2Manager h2Manager;
-        @Override
-        public LuaRunner getLuaRunner() {
-            return luarunner;
-        }
-
-        @Override
-        public LuaLoader getLuaLoader() {
-            return lualoader;
-        }
-
-        @Override
-        public H2Manager getH2Manager() {
-            return h2Manager;
-        }
-        @Override
-        public FileLoader getFileLoader(){
-            return fileLoader;
-        }
-        public void init(){
-            lualoader = new LuaLoader(this);
-            fileLoader = new FileLoader("script");
-            luarunner = new LuaRunner();
-            h2Manager = new H2Manager("./H2/H2", "luascript", "luascript");
-            h2Manager.TryConnect();
-        }
-        public void test(){
-
-        }
-        public void end(){
-            lualoader.close();
-            luarunner.close();
-            h2Manager.CloseConnect();
-        }
-    }
     /**
      * Rigorous Test :-)
      */
@@ -57,10 +18,6 @@ public class LuaTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        TestClass now = new TestClass();
-        now.init();
-        now.test();
-        now.end();
         assertTrue( true );
     }
 }
