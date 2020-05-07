bd83120ec47187dc1ae3397e2f47ff40593c60f6
cxnaive <1012033254@qq.com>
2020-05-07 23:47:24 +0800

    fixlast

--- src/main/java/cxmc/event/EventLoader.java
+++ src/main/java/cxmc/event/EventLoader.java
@@ -8,7 +8,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockBurnEvent;
 import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
